cbf587e7645f4ec0a790d58bc9081984c37bc947
DevelopCarlosWM <laloo_warriors@hotmail.com>
2025-03-30 12:39:58 -0600

    Fix: config

--- dockerfile
+++ dockerfile
@@ -1,33 +1,27 @@
 FROM itzg/minecraft-server
 
-# Basic configuration
 ENV EULA=TRUE
 ENV VERSION=1.18.2
 ENV ONLINE_MODE=false
-
-# Specific configuration for modpacks
 ENV TYPE=FORGE
 ENV CF_SERVER_MOD=https://mediafilez.forgecdn.net/files/5224/123/DawnCraft-Serverpack-2.0.14.zip
-ENV FORGEVERSION=40.2.17  
-
-# Performance settings (recommended memory for modpacks)
+ENV FORGEVERSION=40.2.17
+ENV LEVEL="New World"
+ENV LEVEL_TYPE=DEFAULT
+ENV SEED=
+ENV GENERATE_STRUCTURES=true
+ENV SPAWN_PROTECTION=0
 ENV MEMORY=6G
 
-# Network settings
 EXPOSE 25565
 
-# Persistent volume for Minecraft data
 VOLUME ["/data"]
 
-# Copy the entrypoint script and the world_data folder into the container
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-# Copy the world_data folder into the container (where your world will be stored)
 COPY world_data /world_data
 
-# Set the entrypoint to the custom script
 ENTRYPOINT ["/entrypoint.sh"]
 
-# Default command (Minecraft server start)
 CMD ["/start"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Check if the world folder exists in the /data directory
-if [ ! -d "/data/world" ]; then
-    echo "No world found, copying 'New World' from /world_data..."
+WORLD_NAME="${LEVEL:-"New World"}"
 
-    # Copy the 'New World' directory to /data and rename it to 'world'
-    cp -r /world_data/New\ World /data/world
-
-    echo "'New World' copied successfully as 'world'."
+if [ ! -d "/data/$WORLD_NAME" ]; then
+    java -Xmx$MEMORY -jar /server.jar --level-name="$WORLD_NAME" --level-type="$LEVEL_TYPE" --seed="$SEED" --generate-structures="$GENERATE_STRUCTURES" --spawn-protection="$SPAWN_PROTECTION"
 else
-    echo "World found, starting server..."
+    echo "World '$WORLD_NAME' found, starting server..."
 fi
 
-# Fix the permissions on the world folder to allow Minecraft to write to it
-chown -R minecraft:minecraft /data/world
-chmod -R 755 /data/world
+chown -R minecraft:minecraft "/data/$WORLD_NAME"
+chmod -R 755 "/data/$WORLD_NAME"
 
-# Call the original entrypoint to start the Minecraft server
 exec /start
